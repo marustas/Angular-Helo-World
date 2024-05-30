480d7a4bacd455ec5bef3bddede65cf73438150b
Stanislau Marudau <marudov.stas@gmail.com>
2024-05-30 22:05:13 +0300

    Createed the Product component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/product.service';
-import { Products } from '../../types';
+import { Products, Product } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
-
+  products: Product[] = [];
   ngOnInit() {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 3 })
       .subscribe((products: Products) => {
+        this.products = products.items;
         console.log(products.items);
       });
   }
