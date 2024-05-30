68468c2f87f67da8efadfa0f798e17afa48b9da7
Stanislau Marudau <marudov.stas@gmail.com>
2024-05-30 20:45:29 +0300

    Implemented the necessary styles

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'Footer',
+  selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
