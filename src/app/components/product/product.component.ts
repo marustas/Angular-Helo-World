b2ffbf162b4f2e805538f14fda9573216f75d9ed
Stanislau Marudau <marudov.stas@gmail.com>
2024-05-31 22:31:42 +0300

    Finished the pagination

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,12 +2,11 @@ import { Component, Input } from '@angular/core';
 import { Product } from '../../../types';
 import { FormsModule } from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
-import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [FormsModule, RatingModule, PaginatorModule],
+  imports: [FormsModule, RatingModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,17 +3,20 @@ import { ProductsService } from '../services/product.service';
 import { Products, Product } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
   products: Product[] = [];
+  totalRecords: number = 0;
+  rows: number = 5;
 
   onPageChange(event: any) {
     this.fetchProducts(event.page, event.rows);
@@ -25,10 +28,11 @@ export class HomeComponent {
       .subscribe((products: Products) => {
         this.products = products.items;
         console.log(this.products);
+        this.totalRecords = products.total;
       });
   }
   ngOnInit() {
-    this.fetchProducts(0, 5);
+    this.fetchProducts(0, this.rows);
   }
   // trackByItemId(index: number, product: Product): number {
   //   return product?.id;
